8f34dfb67ef840a404274b53bdd1e8912f2eb6d1
Edward Greenberg <ed.w.greenberg@gmail.com>
2020-09-29 19:18:58 -0400

    image

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import logo from './ENHANCEDREDWHITEWITHAFRICA'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Jasags Creative Media</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Jasags Creative Media Group LLC!
         </h1>
 
+        return <img src={logo} alt="Logo" />;
+
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Powered by Next.js
         </p>
 
         <div className={styles.grid}>
