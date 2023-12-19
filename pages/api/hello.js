9ce665cf68984e32c315992af1cbf70edc065a58
ewgewgewg <ed.w.greenberg@gmail.com>
2023-12-18 19:42:27 -0500

    set runtime edge for cloudflare

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const runtime = 'edge'; // required for cloudflare
+
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
